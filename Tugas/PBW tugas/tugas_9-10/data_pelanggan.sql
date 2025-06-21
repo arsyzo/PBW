3345120d42d68e75d7c4376a64a22052bfff657b
Marsya tri nadiah <168258257+arsyzo@users.noreply.github.com>
2025-06-22 04:20:56 +0700

    Add files via upload

---
+++ Tugas/PBW tugas/tugas_9-10/data_pelanggan.sql
@@ -0,0 +1,6 @@
+INSERT INTO pelanggan (Nama, Alamat, Email, Telepon) VALUES
+('Zora', 'bandung, jawabarat', 'reinara@email.com', '081234567890'),
+('Zoe', 'solo, jawatimur', 'rain@email.com', '081234567891'),
+('Zein', 'cikarang, jawabarat', 'rein@email.com', '081234567892'),
+('Zulfa', 'bekasi, jawabarat', 'rafa@email.com', '081234567893'),
+('Zoro', 'Semarang, jawatengah', 'roronoa@email.com', '081234567894');
